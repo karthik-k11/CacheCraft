6d94873c1a1e0e3d3e2cf727bffa5855c5b14142
Karthik <karthikk1162@gmail.com>
2026-09-02 22:29:16 +0530

    Implemented the LRU cache replacement algorithm

---
+++ algorithms.py
@@ -0,0 +1,25 @@
+from cache import Cache
+
+
+class LRUCache(Cache):
+    def access(self, key):
+        evicted = None
+
+        if self.contains(key):
+            self.items.move_to_end(key)
+            hit = True
+
+        else:
+            hit = False
+
+            if self.is_full():
+                evicted, _ = self.items.popitem(last=False)
+
+            self.add(key)
+
+        return {
+            "request": key,
+            "hit": hit,
+            "evicted": evicted,
+            "cache": self.get_items()
+        }
